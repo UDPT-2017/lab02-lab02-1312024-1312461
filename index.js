384596b5c182329fa4a94a052eceee260c823484
Quan-pc\HuuQuan <nguyenhuuquan0908>
2017-05-22 18:25:25 +0700

    Modified by 1312461

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello NodeJS");
+var express = require("express");
+var app = express();
+
+require('./config')(app);
+//start server
+app.listen(process.env.PORT || 3000, function(){
+      console.log('Example app listening on port 3000!');
+});
